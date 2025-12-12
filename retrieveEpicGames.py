649bcb65a6f4a4c01e7c22d4a1dc8585070abafc
su1y_ <120464982+S3F1RO@users.noreply.github.com>
2025-12-12 14:29:14 -0300

    Enhance Epic Games retrieval logic and message handling
    Added logic to handle URL generation for free games and included a channel purge before sending messages.

--- retrieveEpicGames.py
+++ retrieveEpicGames.py
@@ -5,6 +5,7 @@ from datetime import *
 from discord.ext import tasks
 import os
 from dotenv import load_dotenv
+
 #Bot Initialisation
 load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
@@ -13,28 +14,38 @@ CHANNEL_ID=int(os.getenv("CHANNEL_ID"))
 #Date Checking (if thursday [date of Epic])
 @tasks.loop(hours=24)
 async def sendFreeEpicGames():
-    channel=bot.get_channel(CHANNEL_ID)
-    today=date.today()
-    nextWeek=today + timedelta(days=7)
+    channel = bot.get_channel(CHANNEL_ID)
+    today = date.today()
+    nextWeek = today + timedelta(days=7)
     if today.weekday() == 4 :
         #Link for promotions
-        epic="https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?locale=en-FR&country=FR&allowCountries=FR"
+        epic ="https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?locale=en-FR&country=FR&allowCountries=FR"
         response = requests.get(epic, timeout=10)
         #Get jsonData from link
         data = response.json()
         #Check each elements 
+        await channel.purge(limit=10)
         for i in (data['data']['Catalog']['searchStore']['elements']):
             #Check if the game is free
             if (i['price']['totalPrice']['discountPrice'] == 0):
                 #Check if the title in the URL is None
-                if (i['catalogNs']['mappings'] != None):
-                    urlName=i['catalogNs']['mappings'][0]['pageSlug']
-                    url="https://store.epicgames.com/fr/p/" + urlName
+                if (i['catalogNs']['mappings'] is None): 
+                    urlSlug = i['catalogNs']['mappings'][0]['pageSlug']
+                elif (i['productSlug']):
+                    urlSlug = i['productSlug']
+                else:    
+                    urlSlug = i['urlSlug']
+                
+                #Check if there is a url to send if none don't send
+                if (urlSlug == "[]" or urlSlug is None):
+                    pass
+                else:
+                    url="https://store.epicgames.com/fr/p/" + urlSlug
                     if channel:
                         await channel.send(
-                            f"🎮{i['title']} est gratuit jusqu'au {nextWeek} les gaaaaars !!!!\n"
-                            f"🌐 Lien pour le télécharger :{url}\n\n"    
-		    )
+                            f"🎮 {i['title']} est gratuit jusqu'au {nextWeek} les gaaaaars !!!!\n"
+                            f"🌐 Lien pour le télécharger : {url}\n\n"    
+                        )   
                     else: 
                         print("Channel not found")
 
